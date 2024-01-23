beee100be7f8e383f80cbec2bc66999cac44c29f
VishalGandhe <vishalgandhe9@gmail.com>
2024-01-23 22:33:27 +0530

    Commit

---
+++ src/basics_04/Lab051.java
@@ -0,0 +1,18 @@
+package src.basics_04;
+
+public class Lab051
+{
+    public static void main(String[] args)
+    {
+
+        if(true){
+            System.out.println("1");
+            if(true){
+                System.out.println("2");
+                if(true){
+                    System.out.println("3");
+                }
+            }
+        }
+    }
+}
