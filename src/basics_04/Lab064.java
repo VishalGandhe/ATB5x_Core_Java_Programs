b84c02d64533699b96c14e75b544d41071c478b1
VishalGandhe <vishalgandhe9@gmail.com>
2024-01-25 12:55:06 +0530

    Commit

---
+++ src/basics_04/Lab064.java
@@ -0,0 +1,22 @@
+package src.basics_04;
+
+import java.util.Scanner;
+
+public class Lab064
+{
+    public static void main(String[] args)
+    {
+
+        char code = 'C';
+        int val = switch (code) {
+            case 'A' :
+                yield 65;
+            case 'B' :
+                yield 66;
+            default:
+                throw new IllegalStateException("Unexpected value: " + code);
+        };
+
+        System.out.println(val);
+    }
+}
