c255f7ccc2d686592bd153523a5e94b7c3912058
VishalGandhe <vishalgandhe9@gmail.com>
2024-01-30 22:07:15 +0530

    Commit

---
+++ src/basics_05/Lab076.java
@@ -0,0 +1,22 @@
+package src.basics_05;
+
+public class Lab076
+{
+    public static void main(String[] args)
+    {
+
+        // for( A ;B ; C)
+        // A -> Initialization -> JVM What variable, where you are starting point
+        // B - Condition -> When you want this to stop. -> true, false
+        // C ->  Increment / Decrement
+
+        System.out.println("Print the value of i, but I want to break when i = 5");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Val of " + i);
+
+    }
+
+    }
+}
+
